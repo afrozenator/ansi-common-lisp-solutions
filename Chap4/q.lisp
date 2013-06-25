186e8ed8297495d9d9e2a3099207799a0b10d93b
Afroz Mohiuddin <afrozenator@gmail.com>
2013-06-24 23:25:30 -0700

    new array

--- Chap4/q.lisp
+++ Chap4/q.lisp
@@ -6,10 +6,11 @@
                 (eql (first dims) (second dims)))
            (let ((dim (first dims)))
                 (if (evenp dim)
-                    (quarter-turn-even arr dim)
+                    (quarter-turn-even-new-arr arr dim)
                     (quarter-turn-odd arr dim)))
            nil)))
 
+;; Modifies the array in place.
 (defun quarter-turn-even (arr x)
   (let ((k (/ x 2)))
     (dotimes (ix k)
@@ -23,6 +24,24 @@
 	                   (aref arr (+ j (- k i)) (1- (+ k i))) llj
 		                 (aref arr (1- (+ k i)) (- (1- (+ k i)) j)) tlj
 			               (aref arr (- (1- (+ k i)) j) (- k i)) trj)))))))
+
+;; Makes a new array, which is rotated.
+(defun quarter-turn-even-new-arr (arr x)
+  (let ((k (/ x 2))
+        (narr (make-array (list x x) :initial-element nil)))
+    (dotimes (ix k)
+      (let ((i (1+ ix)))
+        (dotimes (j (1- (* 2 i)))
+          (let ((llj (aref arr (- k i) (+ j (- k i))))
+                (tlj (aref arr (+ j (- k i)) (1- (+ k i))))
+                (trj (aref arr (1- (+ k i)) (- (1- (+ k i)) j)))
+                (lrj (aref arr (- (1- (+ k i)) j) (- k i))))
+               (setf (aref narr (- k i) (+ j (- k i))) lrj
+	                   (aref narr (+ j (- k i)) (1- (+ k i))) llj
+		                 (aref narr (1- (+ k i)) (- (1- (+ k i)) j)) tlj
+			               (aref narr (- (1- (+ k i)) j) (- k i)) trj)))))
+			narr))
+
 
 (defun quarter-turn-odd (arr x)
   arr)
