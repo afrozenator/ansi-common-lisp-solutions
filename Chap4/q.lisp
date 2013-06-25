29f04a8e32a7caaaeeead70ab4ea434113d22ceb
Afroz Mohiuddin <afrozenator@gmail.com>
2013-06-25 09:14:48 -0700

    chap4, all done

--- Chap4/q.lisp
+++ Chap4/q.lisp
@@ -70,3 +70,73 @@
 
 (defun reduce-copy-list (lst)
   (reduce #'(lambda (x y) (cons x y)) lst :from-end t :initial-value nil))
+
+
+;;; 3
+;;;
+;;; Tree with data and upto three children.
+
+(defstruct trinary-node
+	elem
+	(c1 nil)
+	(c2 nil)
+	(c3 nil))
+
+;; There maybe some problem in this, because if I do
+;; (setf x (make-trinary-node :elem 'root :c1 (make-trinary-node :elem 'rc1) :c2 (make-trinary-node :elem 'rc2) :c3 (make-trinary-node :elem 'rc3)))
+;;
+;; and then, (setf y (copy-trinary-node x)) and then try to
+;; (setf (trinary-node-c1 (trinary-node-c1 x)) y)
+;; I get
+;; Program stack overflow. RESET and both x and y are corrupted :?
+(defun copy-trinary-tree (ttree)
+  (if (null ttree)
+      nil
+      (make-trinary-node :elem (trinary-node-elem ttree)
+                         :c1 (copy-trinary-tree (trinary-node-c1 tree))
+                         :c2 (copy-trinary-tree (trinary-node-c2 tree))
+                         :c3 (copy-trinary-tree (trinary-node-c3 tree)))))
+
+(defun object-in-trinary-tree-p (obj ttree)
+  (if (null ttree)
+      nil
+      (if (eql obj (trinary-node-elem ttree))
+          ttree
+          (or (object-in-trinary-tree-p obj (trinary-node-c1 ttree))
+              (object-in-trinary-tree-p obj (trinary-node-c2 ttree))
+              (object-in-trinary-tree-p obj (trinary-node-c3 ttree))))))
+
+;;; 4
+;;;
+;;; In order traversal of a bst
+;; In order traversal was already defined in bst.lisp
+
+
+;;; 5
+;;;
+;;; bst-adjoin, should insert the object iff there is nothing eql to it in the tree.
+;;;
+;;; Assuming bst.lisp has already been 'loaded'.
+(defun bst-adjoin (obj bst <)
+  (if (bst-find bst obj <)
+      bst
+      (bst-insert bst obj <)))
+
+;;; 6
+;;;
+;;; a) assoc-list to hash-table
+;;
+;; how to make an assoc list
+;; (setf a (cons (cons 'a 'b) nil))
+;; (push (cons 'j 'i) a)
+(defun assoc-list-to-hash-table (asclst)
+  (let ((x (make-hash-table)))
+       (dolist (i asclst)
+         (setf (gethash (car i) x) (cdr i)))
+       x))
+
+;;; b) hash-table to assoc list
+(defun hash-table-to-assoc-list (ht)
+  (let ((x nil))
+       (maphash #'(lambda (k v) (push (cons k v) x)) ht)
+       x))
