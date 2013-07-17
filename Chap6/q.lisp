51b68d68fdb6c0106d5b1410bf9ec4f4c1bfd0b1
Afroz Mohiuddin <afrozenator@gmail.com>
2013-07-17 01:13:11 -0700

    .

--- Chap6/q.lisp
+++ Chap6/q.lisp
@@ -18,10 +18,11 @@
 
 ;;; 2
 ;;; bin-search - :key, :test, :start and :end
-;(defun bin-search (seq val &key (key #'identity) (test #'<) (start 0) end)
+;(defun bin-search (seq val &key (key #'identity) (test #'<) (start 0) (end (length seq)))
 ;  (labels ((midpoint (low high) (+ low (floor (/ (- high low) 2)))))
-;    (let ((mid (midpoint start end))
-;          (test-val (funcall test 
+;    (let* ((mid (midpoint start end))
+;           (mid-val (svref seq mid))
+;           ()
 
 ;;; 3
 (defun num-args (&rest args)
@@ -46,3 +47,57 @@
     (t (multiple-value-bind (m1 m2) (most-modified fn (list (first lst) (second lst)))
          (multiple-value-bind (m3 m4) (most-modified fn (cdr lst)) ; we do cdr and not cddr
            (values (my-max m1 m3 :key fn) (my-max m4 (my-max m1 m3 :test #'< :key fn) :key fn)))))))
+
+;;; 5
+;;; remove-if in terms of filter
+;;; What does he mean by no keywords?
+(defun filter (f lst)
+  (let ((accum nil))
+       (dolist (elm lst)
+               (if (not (null (funcall f elm)))
+                   (push elm accum)))
+       (nreverse accum)))
+
+(defun remove-if-modified (pred seq)
+  (filter pred seq))
+
+;;; 6
+;;; A function that returns the greatest argument passed to it so far.
+(let ((max-so-far nil))
+  (defun max-so-far-fn (x)
+    (if (or (null max-so-far)
+            (> x max-so-far))
+        (setf max-so-far x)
+        max-so-far)))
+
+;;; 7
+;;; Return true if this argument is greater than the last argument passed.
+(let ((last-argument nil))
+  (defun was-prev-arg-smaller (x)
+    (if (or (null last-argument)
+            (< last-argument x))
+        (progn (setf last-argument x)
+               t)
+        (progn (setf last-argument x)
+               nil))))
+
+;;; 8
+;;; frugal and expensive
+;;; The idea is to take a function and memoize its result.
+
+;; Sleeps for x seconds and returns x+1
+(defun expensive (x) (progn (sleep x) (1+ x)))
+
+(let ((memoize-hash-table (make-hash-table)))
+  (defun frugal (x)
+    (let ((fx (gethash x memoize-hash-table)))
+      (if (not (null fx))
+          fx
+          (setf (gethash x memoize-hash-table) (expensive x))))))
+
+;;; 9
+;;; Apply and octal
+;;; TODO(afro): Do this
+;(defun apply-octal (fn lst)
+;  (let ((*print-base* 8))
+;    (apply fn lst)))
